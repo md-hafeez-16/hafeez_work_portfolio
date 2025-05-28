5e879667ca8fab48be2fc293e514851f750a9b8a
md-hafeez-16 <mahhmmedhafeez@gmail.com>
2025-05-28 14:12:58 +0530

    Bug fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -74,7 +74,7 @@ const Hero = () => {
 
   const letterVariants = {
     hidden: { opacity: 0, y: 50 },
-    visible: (i) => ({
+    visible: (i: number) => ({
       opacity: 1,
       y: 0,
       transition: {
